b427e9df24782a462020fcfb13b36cd8186e4d74
Your Name <your@email.com>
2025-04-13 16:05:02 +0530

    dynamic generation

--- configs/db.jsx
+++ configs/db.jsx
@@ -1,5 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-const sql = neon(process.env.DATABASE_URL); // ✅ now correct
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL); // ✅ now correct
 export const db = drizzle(sql);
